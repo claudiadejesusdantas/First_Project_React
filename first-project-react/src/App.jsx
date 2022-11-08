2ad5743aaf90920805e6a142553e6e4e251fbfed
claudia de jesus dantas <101577825+claudiadejesusdantas@users.noreply.github.com>
2022-11-07 22:15:31 -0300

    inclusion of biography and cards.

---
+++ first-project-react/src/App.jsx
@@ -0,0 +1,18 @@
+import '../src/App.css'
+import Bio from './Bio'
+import Card from './Card'
+import Cards from './Cards'
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>My favorite programming content</h1>
+      <Bio/>
+      <Cards/>
+    </div>
+  )
+}
+
+export default App
