7943a513c74babb6d9e86eb303c8a45bdc9dc0e4
claudia de jesus dantas <101577825+claudiadejesusdantas@users.noreply.github.com>
2022-11-07 22:14:53 -0300

    inclusion of cards function

---
+++ first-project-react/src/Cards.jsx
@@ -0,0 +1,20 @@
+import Card from "./Card"
+
+function Cards(){
+    return(
+        <div>
+            <Card title="Exemplo 1" paragraph="lorem ipsukan f" link="https://th.bing.com/th/id/OIP.3kIWpW_L7vqchblE5DgcxwHaHa?pid=ImgDet&rs=1" alt="parrot"/ >
+
+        
+            <Card title="Exemplo 1" paragraph="ddddddd" link="https://th.bing.com/th/id/OIP.3kIWpW_L7vqchblE5DgcxwHaHa?pid=ImgDet&rs=1" alt="parrot"/ >
+
+            <Card title="Exemplo 1" paragraph="lorem ipsukan f" link="https://th.bing.com/th/id/OIP.3kIWpW_L7vqchblE5DgcxwHaHa?pid=ImgDet&rs=1" alt="parrot"/ >
+
+            <Card title="Exemplo 1" paragraph="lorem ipsukan f" link="https://th.bing.com/th/id/OIP.3kIWpW_L7vqchblE5DgcxwHaHa?pid=ImgDet&rs=1" alt="parrot"/ >
+
+            <Card title="Exemplo 1" paragraph="lorem ipsukan f" link="https://th.bing.com/th/id/OIP.3kIWpW_L7vqchblE5DgcxwHaHa?pid=ImgDet&rs=1" alt="parrot"/ >
+        </div>
+    )
+}
+
+export default Cards
