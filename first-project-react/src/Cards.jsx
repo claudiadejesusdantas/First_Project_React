bd07235a70b96138db73141d57baeda30f908fcc
claudia de jesus dantas <101577825+claudiadejesusdantas@users.noreply.github.com>
2022-11-09 08:46:36 -0300

    included css card

--- first-project-react/src/Cards.jsx
+++ first-project-react/src/Cards.jsx
@@ -7,13 +7,13 @@ function Cards(){
                 title="Git & Github" 
                 paragraph="I started my tech journey by learning about Git, whinch is a code versioning system, where I can share my code, track changes and roll back them, alone or in a group. The platform I used upload my projects and push my git was Github. " 
                 link="https://media1.giphy.com/media/l0K4nxdKgRzEZu47u/giphy.gif?cid=ecf05e4728hlcmwssf9jusfqxkjjvs09mkhjcpvjwkpurl6c&rid=giphy.gif&ct=g" 
-                alt="parrot"/ >
+                alt="a mushroom, a flower and a star being exchanged for their parts."/ >
 
             <Card 
-                title="Exemplo 2" 
-                paragraph="ddddddd" 
-                link="https://th.bing.com/th/id/OIP.3kIWpW_L7vqchblE5DgcxwHaHa?pid=ImgDet&rs=1" 
-                alt="parrot"/ >
+                title="CSS" 
+                paragraph="CSS(Cascading Style Sheets) is a style language, used to style pages. Some very important tools to learn were display grid and display flex, for positioning elements on screen. In addition to working with @media query, for mobile responsiveness." 
+                link="https://media4.giphy.com/media/12QMzVeF4QsqTC/giphy.gif?cid=ecf05e47fdqqz9o14lc8sqhd3nkr65bsysm25nt7yrxktgo1&rid=giphy.gif&ct=g" 
+                alt="video game Mario changing clothes."/ >
 
             <Card 
                 title="Exemplo 3" 
