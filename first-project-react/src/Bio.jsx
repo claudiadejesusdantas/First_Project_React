c0bbf41b32176c10757945f0f0c6c341370f14e4
claudia de jesus dantas <101577825+claudiadejesusdantas@users.noreply.github.com>
2022-11-09 08:35:59 -0300

    modified for Bio

--- first-project-react/src/Bio.jsx
+++ first-project-react/src/Bio.jsx
@@ -5,8 +5,8 @@ import Subtitle from "./Subtitle"
 function Bio(){
     return(
         <header>
-            <Subtitle text="Nice to meet you, I'm Claudia de Jesus Dantas"/>
-            <Paragraph text="I have a degree in Human Resources, currently studying technology. Passionate about the front-end area, Reprograma student and instagram content creator @claudiadejesusdantastudy."/>
+            <Subtitle text="Nice to meet you, I'm Claudia DJ Dantas"/>
+            <Paragraph text="I have a degree in Human Resources, currently studying technology. Passionate about front-end, video games, romance books and horror movies. Reprograma student and instagram content creator @claudiadejesusdantastudy."/>
             <Photo src="https://avatars.githubusercontent.com/u/101577825?v=4" alt="Photo for Claudia de Jesus Dantas"/>
         </header>
     )
