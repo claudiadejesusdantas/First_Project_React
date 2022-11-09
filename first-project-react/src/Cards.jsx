9c69214241c22adf3b6ee6b1a49cc2cc7eb32a3c
claudia de jesus dantas <101577825+claudiadejesusdantas@users.noreply.github.com>
2022-11-09 08:33:51 -0300

    included card for git

--- first-project-react/src/Cards.jsx
+++ first-project-react/src/Cards.jsx
@@ -2,17 +2,37 @@ import Card from "./Card"
 
 function Cards(){
     return(
-        <div>
-            <Card title="Exemplo 1" paragraph="lorem ipsukan f" link="https://th.bing.com/th/id/OIP.3kIWpW_L7vqchblE5DgcxwHaHa?pid=ImgDet&rs=1" alt="parrot"/ >
+        <section>
+            <Card 
+                title="Git & Github" 
+                paragraph="I started my tech journey by learning about Git, whinch is a code versioning system, where I can share my code, track changes and roll back them, alone or in a group. The platform I used upload my projects and push my git was Github. " 
+                link="https://media1.giphy.com/media/l0K4nxdKgRzEZu47u/giphy.gif?cid=ecf05e4728hlcmwssf9jusfqxkjjvs09mkhjcpvjwkpurl6c&rid=giphy.gif&ct=g" 
+                alt="parrot"/ >
 
-            <Card title="Exemplo 2" paragraph="ddddddd" link="https://th.bing.com/th/id/OIP.3kIWpW_L7vqchblE5DgcxwHaHa?pid=ImgDet&rs=1" alt="parrot"/ >
+            <Card 
+                title="Exemplo 2" 
+                paragraph="ddddddd" 
+                link="https://th.bing.com/th/id/OIP.3kIWpW_L7vqchblE5DgcxwHaHa?pid=ImgDet&rs=1" 
+                alt="parrot"/ >
 
-            <Card title="Exemplo 3" paragraph="lorem ipsukan f" link="https://th.bing.com/th/id/OIP.3kIWpW_L7vqchblE5DgcxwHaHa?pid=ImgDet&rs=1" alt="parrot"/ >
+            <Card 
+                title="Exemplo 3" 
+                paragraph="lorem ipsukan f" 
+                link="https://th.bing.com/th/id/OIP.3kIWpW_L7vqchblE5DgcxwHaHa?pid=ImgDet&rs=1" 
+                alt="parrot"/ >
 
-            <Card title="Exemplo 4" paragraph="lorem ipsukan f" link="https://th.bing.com/th/id/OIP.3kIWpW_L7vqchblE5DgcxwHaHa?pid=ImgDet&rs=1" alt="parrot"/ >
+            <Card 
+                title="Exemplo 4" 
+                paragraph="lorem ipsukan f" 
+                link="https://th.bing.com/th/id/OIP.3kIWpW_L7vqchblE5DgcxwHaHa?pid=ImgDet&rs=1" 
+                alt="parrot"/ >
 
-            <Card title="Exemplo 5" paragraph="lorem ipsukan f" link="https://th.bing.com/th/id/OIP.3kIWpW_L7vqchblE5DgcxwHaHa?pid=ImgDet&rs=1" alt="parrot"/ >
-        </div>
+            <Card 
+                title="Exemplo 5" 
+                paragraph="lorem ipsukan f" 
+                link="https://th.bing.com/th/id/OIP.3kIWpW_L7vqchblE5DgcxwHaHa?pid=ImgDet&rs=1" 
+                alt="parrot"/ >
+        </section>
     )
 }
 
